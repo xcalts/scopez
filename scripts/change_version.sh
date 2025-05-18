b9ecd8dafd478d490c47ffc86c7b01632914014b
xcalts <christos@xcalts.co>
2025-05-18 15:38:49 +0300

    feat: v0.0.4

--- scripts/change_version.sh
+++ scripts/change_version.sh
@@ -11,10 +11,8 @@ NEW_VERSION="$1"
 
 # Update pyproject.toml
 sed -i '' -E "s/^version = \".*\"/version = \"$NEW_VERSION\"/" pyproject.toml
-
 # Update setup_cx.py
 sed -i '' -E "s/(version=)\"[^\"]+\"/\1\"$NEW_VERSION\"/" setup_cx.py
-
 # Update src/__version__.py
 sed -i '' -E "s/^__version__ = \".*\"/__version__ = \"$NEW_VERSION\"/" src/__version__.py
 
@@ -23,9 +21,9 @@ echo "Version updated to $NEW_VERSION in:
 - setup_cx.py
 - src/__version__.py"
 
-echo "Pushing to Github"
 git add -A
 git commit -m "feat: $NEW_VERSION"
 git push origin main
+
 git tag -a "$NEW_VERSION" -m "$NEW_VERSION"
 git push origin "$NEW_VERSION"
